93c24be2a99fc93cbc2e33bb600b8fc48a07a096
Kuei <material3679@gmail.com>
2020-04-23 00:29:30 +0800

    exercise of react router: show, rmtp live stream server

--- streams/client/src/components/streams/StreamShow.js
+++ streams/client/src/components/streams/StreamShow.js
@@ -1,11 +1,57 @@
 import React from "react"
+import flv from "flv.js"
+import {fetchStream} from "../../actions/index.js"
+import {connect} from "react-redux"
 
-const StreamShow = ()=>{
-    return (
-        <div>
-            StreamShow
-        </div>
-    )
+class StreamShow extends React.Component{
+    constructor(props){
+        super(props)
+
+        this.videoRef = React.createRef()
+    }
+    componentDidMount(){
+        this.props.fetchStream(this.props.match.params.id)
+        this.buildPlayer()
+    }
+
+    buildPlayer(){
+        if (this.player || this.props.stream){
+            this.player = flv.createPlayer({
+                type: "flv",
+                url: `http://localhost:8000/live/${this.props.match.params.id}.flv`
+            })
+            this.player.attachMediaElement(this.videoRef.current)
+            this.player.load()
+        }
+    }
+
+    componentDidUpdate(){
+        this.buildPlayer()
+    }
+
+    componentWillUnmount(){
+        this.player.destroy()
+    }
+
+    render(){
+        if (!this.props.stream){
+            return (<div>Loading...</div>)
+        } 
+
+        const {title,description} = this.props.stream
+
+        return (
+            <div>
+                <video ref={this.videoRef} style={{width: "100%"}} controls="true" />
+                <h1>{title}</h1>
+                <h5>{description}</h5>
+            </div>
+        )
+    }
 }
 
-export default StreamShow
+
+const mapStateToProps = (state,ownProps)=>{
+    return {stream: state.streams[ownProps.match.params.id]}
+}
+export default connect(mapStateToProps,{fetchStream:fetchStream})(StreamShow)
